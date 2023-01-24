0bb3e44401876da4d90f09c87bc0be32318950f1
TereLozaIteso <tereloza@iteso.mx>
2023-01-24 17:20:59 -0600

    Archivo vacio para primer proyecto diplomado

---
+++ punto_venta.py
@@ -0,0 +1,35 @@
+'''
+Fecha encargo: 24/01/2023
+Fecha entrega: 
+Nombre desarrollador:
+
+Descripción: realice un algoritmo y código Python que:
+Capture una cantidad indeterminada de precios de productos sin iva;
+la captura terminará hasta que el usuario del sistema indique “fin de captura”. 
+El fin de captura, lo indicará mediante un “valor centinela” que será un número 
+negativo en la entrada de datos.
+
+Pida confirmación sólo cuando cuando se ingrese el negativo ¿seguro de cerrar la cuenta?
+
+Entregue como salida del sistema: 
+* La cantidad de precios de productos introducidos
+* El total del valor de los productos antes de IVA (subtotal)
+* El IVA a agregar, y
+* El total con IVA.
+Alineé las cantidades de salida para fácil lectura, vea el ejemplo en imagen incluido en el repositorio.
+
+------------
+**Para quienes tienen más conocimiento de Python, puede agregar en la entrada
+Nombre de cada producto a capturar, su costo unitario y cantidad de producto a comprar, por cada uno
+
+Y en la salida:
+Tabla con los nombres de los productos y sus costos unitarios, cantidad y costo por producto*cantidad 
+------------
+
+Se espera evaluar: uso de ciclo mientras (while); repetición controlada con centinela; uso de contadores; 
+impresión con formato.
+
+Al final, agregue un comentario con una reflexión de los temas usados hasta el momento y su experiencia,
+fácilidad o dificultad para integrarlos.
+
+'''
